e5cd25bcdd52a5bbbe71cea978e3fd1d7c76964d
yuioo436 <ay62050380@gmail.com>
2025-06-28 10:51:57 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAk2lv6TfKIiptdEKooSU0NmEde94R2EHM",
+  authDomain: "uttt-f1f02.firebaseapp.com",
+  databaseURL: "https://uttt-f1f02-default-rtdb.firebaseio.com",
+  projectId: "uttt-f1f02",
+  storageBucket: "uttt-f1f02.firebasestorage.app",
+  messagingSenderId: "243007886313",
+  appId: "1:243007886313:web:d4cf31cbf44faa0c207178",
+  measurementId: "G-D4GDK6NXZ1"
+};
+firebase.initializeApp(firebaseConfig);
